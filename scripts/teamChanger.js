5f8d278f2903a09188e67bf69063076825a0049a
Jonah Stanley <swimming95@aol.com>
2012-07-10 21:07:20 -0400

    New Structure

--- scripts/teamChanger.js
+++ scripts/teamChanger.js
@@ -2,7 +2,11 @@
 DATA
 */
 
-//A Team is defined as an array of 6 (or less) Pokemon
+//pokemon is an array of up to 6 pokemon
+var Team = function (pokemon){
+	this.pokemon=pokemon;
+};
+
 //All of the information is stored as numbers
 var Pokemon = function (num,nick,gender,ability,item,level,ivs,evs,moves,nature){
 	this.num=num;
@@ -15,7 +19,7 @@ var Pokemon = function (num,nick,gender,ability,item,level,ivs,evs,moves,nature)
 	this.evs=evs;
 	this.moves=moves;
 	this.nature=nature;
-}
+};
 
 //IVs are numbers between 0 and 31
 var IVs = function(hp,att,def,spAtt,spDef,speed){
@@ -25,7 +29,7 @@ var IVs = function(hp,att,def,spAtt,spDef,speed){
 	this.spAtt=spAtt;
 	this.spDef=spDef;
 	this.speed=speed;
-}
+};
 
 //EVs are numbers between 0 and 31
 var EVs = function(hp,att,def,spAtt,spDef,speed){
@@ -35,7 +39,7 @@ var EVs = function(hp,att,def,spAtt,spDef,speed){
 	this.spAtt=spAtt;
 	this.spDef=spDef;
 	this.speed=speed;
-}
+};
 
 /*
 FUNCTIONS
@@ -45,15 +49,7 @@ FUNCTIONS
 This function takes in a team slot number and teamData
 At the end of this function, the user's team at the given slot number will be set to the given teamData
 */
-User.prototype.setTeam = function(slot, teamArray){}
-
-
-
-/*
-This function will take in a team slot number, a pokemon slot number and pokemonData
-At the end of this function, the pokemon in the given poke slot number in the given team slot number will be the given Pokemon
-*/
-User.prototype.setPokemon = function (teamSlot,pokeSlot,poke){}
+User.prototype.setTeam = function(teamArray){}
 
 
 /*
@@ -62,8 +58,8 @@ This function will return the team array equivilent of the given team slot numbe
 User.prototype.getTeam = function (slot){}
 
 
+Team.prototype.syncToUser=function(id){}
 
-/*
-This function will return the Pokemon equivalent of the given pokemon slot number in the given team slot number
-*/
-User.prototype.getPokemon = function (teamSlot, pokeSlot){}
+
+
+Pokemon.prototype.syncToUser=function(id,slot){}
