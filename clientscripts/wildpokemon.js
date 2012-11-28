6aefe196dddd88423f44227e27c25a33d6078276
coyotte508 <coyotte508@hotmail.com>
2012-11-28 19:45:47 +0100

    Added script for a wild pokemon
    When you meet a wild pokemon, the server will force
    a battle between you and a client "Wild Pokemon", setting
    the team of "Wild Pokemon" to one wild pokemon.
    
    The wild pokemon client will have that script so it
    battles randomly. (it will also have a script to close
    the battle window when it's finished...)

---
+++ clientscripts/wildpokemon.js
@@ -0,0 +1,28 @@
+var poke = function(spot) { return battle.data.team(spot).poke(0);};
+var fpoke = function(spot) { return battle.data.field.poke(spot);};
+var tpoke = function(ind) { return battle.data.team(battle.me).poke(ind);};
+
+({
+onChoiceSelection: function(player) {
+    if (player!=battle.me || !useAI) {
+        return;
+    }
+    var switches = [];
+    for (var i = 1; i < 6; i++) {
+        if (!tpoke(i).isKoed()) {
+           switches.push(i);
+        }
+    }
+
+   var r = sys.rand(0, 8);
+
+    if (r == 0 || (fpoke(battle.me).onTheField && !poke(battle.me).isKoed() && (r != 1 || switches.length == 0))) {
+        choice = {"slot": battle.me, "type":"attack", "attackSlot":sys.rand(0,4)};
+    } else {
+        var cswitch = switches[sys.rand(0,switches.length)];
+   
+        choice = {"slot": battle.me, "type":"switch", "pokeSlot": cswitch};
+    }
+    battle.battleCommand(battle.id, choice);
+}
+})
